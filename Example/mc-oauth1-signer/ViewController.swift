db08009deddbf1e068430f9ad056487631365fd5
Luke Reichold <lukereichold@gmail.com>
2018-12-13 14:31:58 -0600

    Make generic

--- Example/mc-oauth1-signer/ViewController.swift
+++ Example/mc-oauth1-signer/ViewController.swift
@@ -7,25 +7,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        ***REMOVED***()
+        requestService()
     }
 
-    func ***REMOVED***() {
-        let urlString = "***REMOVED***"
+    func requestService() {
+        let urlString = "https://sandbox.api.mastercard.com/service"
         let method = "POST";
         let uri = URL(string: urlString)!
+        let consumerKey = "<<REPLACE_ME>>"
         
-        let consumerKey = "***REMOVED***"
-        let ***REMOVED*** = ***REMOVED*** // ***REMOVED***
-        let apiSecretKey = "***REMOVED***" // ***REMOVED***
-        
-        let timestamp = currentUnixTimestamp()
-        let rawSignature = timestamp + "_" + String(***REMOVED***) + "_" + apiSecretKey
-        let signatureString = rawSignature.sha256()?.hexString() ?? ""
-        
-        let payload: Parameters = ["***REMOVED***": ***REMOVED***,
-                                   "time": Int(timestamp)!,
-                                   "signature": signatureString,
+        let payload: Parameters = [
                                    "languageId": 1,
                                    "geographicId": 0]
         
@@ -43,20 +34,6 @@ class ViewController: UIViewController {
         
         Alamofire.request(urlString, method: .post, parameters: payload, encoding: JSONEncoding.default, headers: headers).responseJSON {
             response in
-            
-            // DEBUGGING
-            
-            print("Raw signature:")
-            debugPrint(rawSignature)
-            
-            print("\nSHA256 hashed signature (URL encoded):")
-            debugPrint(signatureString)
-            
-            print("\nPrinting raw UrlRequest in cURL format...")
-            debugPrint(NSString(string: response.request!.cURL))
-            
-            // -- END DEBUGGING
-            
             switch response.result {
             case .success:
                 print(response)
@@ -67,81 +44,9 @@ class ViewController: UIViewController {
     }
     
     private func getPrivateKey() -> SecKey {
-        let certificatePath = Bundle(for: ViewController.self).path(forResource: "***REMOVED***", ofType: "p12")
+        let certificatePath = Bundle(for: ViewController.self).path(forResource: "<<REPLACE_ME>>", ofType: "p12")
         
-        let signingKey = KeyProvider.loadPrivateKey(fromPath: certificatePath!, keyPassword: "***REMOVED***")!
+        let signingKey = KeyProvider.loadPrivateKey(fromPath: certificatePath!, keyPassword: "<<REPLACE_ME>>")!
         return signingKey
-    }
-    
-    func currentUnixTimestamp() -> String {
-        return String(Int(Date().timeIntervalSince1970))
-    }
-    
-}
-
-public extension URLRequest {
-    
-    /// Returns a cURL command for a request
-    /// - return A String object that contains cURL command or "" if an URL is not properly initalized.
-    public var cURL: String {
-        
-        guard
-            let url = url,
-            let httpMethod = httpMethod,
-            url.absoluteString.utf8.count > 0
-            else {
-                return ""
-        }
-        
-        var curlCommand = "curl --verbose \\\n"
-        
-        // URL
-        curlCommand = curlCommand.appendingFormat(" '%@' \\\n", url.absoluteString)
-        
-        // Method if different from GET
-        if "GET" != httpMethod {
-            curlCommand = curlCommand.appendingFormat(" -X %@ \\\n", httpMethod)
-        }
-        
-        // Headers
-        let allHeadersFields = allHTTPHeaderFields!
-        let allHeadersKeys = Array(allHeadersFields.keys)
-        let sortedHeadersKeys  = allHeadersKeys.sorted(by: <)
-        for key in sortedHeadersKeys {
-            curlCommand = curlCommand.appendingFormat(" -H '%@: %@' \\\n", key, self.value(forHTTPHeaderField: key)!)
-        }
-        
-        // HTTP body
-        if let httpBody = httpBody, httpBody.count > 0 {
-            let httpBodyString = String(data: httpBody, encoding: String.Encoding.utf8)!
-            let escapedHttpBody = URLRequest.escapeAllSingleQuotes(httpBodyString)
-            curlCommand = curlCommand.appendingFormat(" --data '%@' \\\n", escapedHttpBody)
-        }
-        
-        return curlCommand
-    }
-    
-    /// Escapes all single quotes for shell from a given string.
-    static func escapeAllSingleQuotes(_ value: String) -> String {
-        return value.replacingOccurrences(of: "'", with: "'\\''")
-    }
-}
-
-extension String {
-    func sha256() -> Data? {
-        guard let data = data(using: String.Encoding.utf8) else { return nil }
-        return data.sha256()
-    }
-}
-
-extension Data {
-    func hexString() -> String {
-        return map { String(format: "%02hhx", $0) }.joined()
-    }
-    
-    func sha256() -> Data? {
-        guard let res = NSMutableData(length: Int(CC_SHA256_DIGEST_LENGTH)) else { return nil }
-        CC_SHA256((self as NSData).bytes, CC_LONG(self.count), res.mutableBytes.assumingMemoryBound(to: UInt8.self))
-        return res as Data
     }
 }
